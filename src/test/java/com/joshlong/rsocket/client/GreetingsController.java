4bc5aef4d8313ce18344486593e77c6d21ea5768
Josh Long <josh@joshlong.com>
2020-07-18 04:05:46 -0700

    up

--- src/test/java/com/joshlong/rsocket/client/GreetingsController.java
+++ src/test/java/com/joshlong/rsocket/client/GreetingsController.java
@@ -18,50 +18,51 @@ import java.util.stream.Stream;
 @Controller
 class GreetingsController {
 
-    static AtomicReference<String> fireAndForget = new AtomicReference<>();
+	static AtomicReference<String> fireAndForget = new AtomicReference<>();
 
-    @PostConstruct
-    public void begin() {
-        log.info("begin()");
-    }
+	@PostConstruct
+	public void begin() {
+		log.info("begin()");
+	}
 
-    @MessageMapping("greetings-mono-name.{name}.{age}")
-    Mono<String> greetMonoNameDestinationVariable(@DestinationVariable("name") String name,
-                                                  @DestinationVariable("age") int age, @Payload Mono<String> payload) {
-        log.info("name=" + name);
-        log.info("age=" + age);
-        return payload;
-    }
+	@MessageMapping("greetings-mono-name.{name}.{age}")
+	Mono<String> greetMonoNameDestinationVariable(@DestinationVariable("name") String name,
+			@DestinationVariable("age") int age, @Payload Mono<String> payload) {
+		log.info("name=" + name);
+		log.info("age=" + age);
+		return payload;
+	}
 
-    @MessageMapping("fire-and-forget")
-    Mono<Void> fireAndForget(Mono<String> valueIn) {
-        return valueIn//
-                .doOnNext(value -> {
-                    log.info("received fire-and-forget " + value + '.');
-                    fireAndForget.set(value);
-                })//
-                .then();
-    }
+	@MessageMapping("fire-and-forget")
+	Mono<Void> fireAndForget(Mono<String> valueIn) {
+		return valueIn//
+				.doOnNext(value -> {
+					log.info("received fire-and-forget " + value + '.');
+					fireAndForget.set(value);
+				})//
+				.then();
+	}
 
-    @MessageMapping("greetings-with-channel")
-    Flux<GreetingResponse> greetParams(Flux<String> names) {
-        return names.map(String::toUpperCase).map(GreetingResponse::new);
-    }
+	@MessageMapping("greetings-with-channel")
+	Flux<GreetingResponse> greetParams(Flux<String> names) {
+		return names.map(String::toUpperCase).map(GreetingResponse::new);
+	}
 
-    @MessageMapping("greetings-stream")
-    Flux<GreetingResponse> greetFlux(Mono<String> name) {
-        return name.flatMapMany(leNom -> Flux.fromStream(Stream.generate(() -> new GreetingResponse(leNom.toUpperCase()))).take(2));
-    }
+	@MessageMapping("greetings-stream")
+	Flux<GreetingResponse> greetFlux(Mono<String> name) {
+		return name.flatMapMany(
+				leNom -> Flux.fromStream(Stream.generate(() -> new GreetingResponse(leNom.toUpperCase()))).take(2));
+	}
 
-    @MessageMapping("greetings-with-name")
-    Mono<GreetingResponse> greetMono(Mono<String> name) {
-        return name.map(GreetingResponse::new);
-    }
+	@MessageMapping("greetings-with-name")
+	Mono<GreetingResponse> greetMono(Mono<String> name) {
+		return name.map(GreetingResponse::new);
+	}
 
-    @MessageMapping("greetings")
-    Mono<GreetingResponse> greet() {
-        log.info("invoking greetings and returning a GreetingsResponse.");
-        return Mono.just(new GreetingResponse("Hello, world!"));
-    }
+	@MessageMapping("greetings")
+	Mono<GreetingResponse> greet() {
+		log.info("invoking greetings and returning a GreetingsResponse.");
+		return Mono.just(new GreetingResponse("Hello, world!"));
+	}
 
 }
--- src/test/java/com/joshlong/rsocket/client/RSocketClientTest.java
+++ src/test/java/com/joshlong/rsocket/client/RSocketClientTest.java
@@ -26,98 +26,98 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Log4j2
 public class RSocketClientTest {
 
-    static ConfigurableApplicationContext serviceApplicationContext;
+	static ConfigurableApplicationContext serviceApplicationContext;
 
-    static AtomicInteger port = new AtomicInteger(SocketUtils.findAvailableTcpPort());
+	static AtomicInteger port = new AtomicInteger(SocketUtils.findAvailableTcpPort());
 
-    @BeforeAll
-    public static void begin() throws Exception {
-        serviceApplicationContext = new SpringApplicationBuilder(RSocketServerConfiguration.class)//
-                .web(WebApplicationType.NONE)
-                .run("--spring.profiles.active=service", "--spring.rsocket.server.port=" + port.get());
-    }
+	@BeforeAll
+	public static void begin() throws Exception {
+		serviceApplicationContext = new SpringApplicationBuilder(RSocketServerConfiguration.class)//
+				.web(WebApplicationType.NONE)
+				.run("--spring.profiles.active=service", "--spring.rsocket.server.port=" + port.get());
+	}
 
-    @AfterAll
-    public static void destroy() {
-        serviceApplicationContext.stop();
-    }
+	@AfterAll
+	public static void destroy() {
+		serviceApplicationContext.stop();
+	}
 
-    @Test
-    public void destinationVariablesAndPayload() throws Exception {
-        GreetingClient greetingClient = buildClient();
-        Mono<String> greetingResponseFlux = greetingClient.greetMonoNameDestinationVariable("jlong", 36, Mono.just("Hello"));
-        StepVerifier//
-                .create(greetingResponseFlux)//
-                .expectNextMatches(gr -> gr.equalsIgnoreCase("Hello"))//
-                .verifyComplete();
-    }
+	@Test
+	public void destinationVariablesAndPayload() throws Exception {
+		GreetingClient greetingClient = buildClient();
+		Mono<String> greetingResponseFlux = greetingClient.greetMonoNameDestinationVariable("jlong", 36,
+				Mono.just("Hello"));
+		StepVerifier//
+				.create(greetingResponseFlux)//
+				.expectNextMatches(gr -> gr.equalsIgnoreCase("Hello"))//
+				.verifyComplete();
+	}
 
-    @Test
-    public void monoInFluxOut() {
-        GreetingClient greetingClient = buildClient();
-        Flux<GreetingResponse> greetingResponseFlux = greetingClient.greetStream(Mono.just("a"));
-        StepVerifier//
-                .create(greetingResponseFlux)//
-                .expectNextCount(1)
-                .expectNextMatches(gr -> gr.getMessage().equalsIgnoreCase("A"))//
-                .verifyComplete();
+	@Test
+	public void monoInFluxOut() {
+		GreetingClient greetingClient = buildClient();
+		Flux<GreetingResponse> greetingResponseFlux = greetingClient.greetStream(Mono.just("a"));
+		StepVerifier//
+				.create(greetingResponseFlux)//
+				.expectNextCount(1).expectNextMatches(gr -> gr.getMessage().equalsIgnoreCase("A"))//
+				.verifyComplete();
 
-    }
+	}
 
-    @Test
-    public void fluxInFluxOut() {
-        GreetingClient greetingClient = buildClient();
-        Flux<GreetingResponse> greetingResponseFlux = greetingClient.greetParams(Flux.just("a", "b"));
-        StepVerifier//
-                .create(greetingResponseFlux)//
-                .expectNextMatches(gr -> gr.getMessage().equalsIgnoreCase("A"))//
-                .expectNextMatches(gr -> gr.getMessage().equalsIgnoreCase("B"))//
-                .verifyComplete();
+	@Test
+	public void fluxInFluxOut() {
+		GreetingClient greetingClient = buildClient();
+		Flux<GreetingResponse> greetingResponseFlux = greetingClient.greetParams(Flux.just("a", "b"));
+		StepVerifier//
+				.create(greetingResponseFlux)//
+				.expectNextMatches(gr -> gr.getMessage().equalsIgnoreCase("A"))//
+				.expectNextMatches(gr -> gr.getMessage().equalsIgnoreCase("B"))//
+				.verifyComplete();
 
-    }
+	}
 
-    @Test
-    public void monoInMonoOut() {
-        GreetingClient greetingClient = buildClient();
-        Mono<GreetingResponse> greet = greetingClient.greet(Mono.just("Hello, Mario"));
-        StepVerifier//
-                .create(greet)//
-                .expectNextMatches(gr -> gr.getMessage().equalsIgnoreCase("Hello, Mario"))//
-                .verifyComplete();
+	@Test
+	public void monoInMonoOut() {
+		GreetingClient greetingClient = buildClient();
+		Mono<GreetingResponse> greet = greetingClient.greet(Mono.just("Hello, Mario"));
+		StepVerifier//
+				.create(greet)//
+				.expectNextMatches(gr -> gr.getMessage().equalsIgnoreCase("Hello, Mario"))//
+				.verifyComplete();
 
-    }
+	}
 
-    @Test
-    public void noValueInMonoOut() throws Exception {
-        GreetingClient greetingClient = buildClient();
-        Mono<GreetingResponse> greet = greetingClient.greet();
-        StepVerifier//
-                .create(greet)//
-                .expectNextMatches(gr -> gr.getMessage().equalsIgnoreCase("Hello, world!"))//
-                .verifyComplete();
+	@Test
+	public void noValueInMonoOut() throws Exception {
+		GreetingClient greetingClient = buildClient();
+		Mono<GreetingResponse> greet = greetingClient.greet();
+		StepVerifier//
+				.create(greet)//
+				.expectNextMatches(gr -> gr.getMessage().equalsIgnoreCase("Hello, world!"))//
+				.verifyComplete();
 
-    }
+	}
 
-    @Test
-    public void fireAndForget() throws Exception {
-        GreetingClient greetingClient = buildClient();
-        String name = "Kimly";
-        Mono<Void> greet = greetingClient.greetFireAndForget(Mono.just(name));
-        StepVerifier//
-                .create(greet)//
-                .verifyComplete();
+	@Test
+	public void fireAndForget() throws Exception {
+		GreetingClient greetingClient = buildClient();
+		String name = "Kimly";
+		Mono<Void> greet = greetingClient.greetFireAndForget(Mono.just(name));
+		StepVerifier//
+				.create(greet)//
+				.verifyComplete();
 
-        Thread.sleep(1000);
-        boolean kimly = GreetingsController.fireAndForget.get().equalsIgnoreCase(name);
-        Assertions.assertTrue(kimly, "the name perceived on the service is the same as we sent.");
-    }
+		Thread.sleep(1000);
+		boolean kimly = GreetingsController.fireAndForget.get().equalsIgnoreCase(name);
+		Assertions.assertTrue(kimly, "the name perceived on the service is the same as we sent.");
+	}
 
-    private GreetingClient buildClient() {
-        ConfigurableApplicationContext context = new SpringApplicationBuilder(RSocketClientConfiguration.class)//
-                .web(WebApplicationType.NONE)//
-                .run("--service.port=" + port.get(), "--spring.profiles.active=client");
-        return context.getBean(GreetingClient.class);
-    }
+	private GreetingClient buildClient() {
+		ConfigurableApplicationContext context = new SpringApplicationBuilder(RSocketClientConfiguration.class)//
+				.web(WebApplicationType.NONE)//
+				.run("--service.port=" + port.get(), "--spring.profiles.active=client");
+		return context.getBean(GreetingClient.class);
+	}
 
 }
 
@@ -127,15 +127,15 @@ public class RSocketClientTest {
 @EnableRSocketClients
 class RSocketClientConfiguration {
 
-    @PostConstruct
-    public void start() {
-        log.info("starting " + RSocketClientConfiguration.class.getName() + '.');
-    }
+	@PostConstruct
+	public void start() {
+		log.info("starting " + RSocketClientConfiguration.class.getName() + '.');
+	}
 
-    @Bean
-    RSocketRequester rSocketRequester(@Value("${service.port}") int port, RSocketRequester.Builder builder) {
-        return builder.connectTcp("localhost", port).block();
-    }
+	@Bean
+	RSocketRequester rSocketRequester(@Value("${service.port}") int port, RSocketRequester.Builder builder) {
+		return builder.connectTcp("localhost", port).block();
+	}
 
 }
 
@@ -145,14 +145,14 @@ class RSocketClientConfiguration {
 @EnableAutoConfiguration
 class RSocketServerConfiguration {
 
-    @Bean
-    GreetingsController greetingsController() {
-        return new GreetingsController();
-    }
+	@Bean
+	GreetingsController greetingsController() {
+		return new GreetingsController();
+	}
 
-    @PostConstruct
-    public void start() {
-        log.info("starting " + RSocketServerConfiguration.class.getName() + '.');
-    }
+	@PostConstruct
+	public void start() {
+		log.info("starting " + RSocketServerConfiguration.class.getName() + '.');
+	}
 
 }
